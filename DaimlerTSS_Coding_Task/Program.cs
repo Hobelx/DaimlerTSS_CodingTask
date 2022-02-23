6c849bbf18ea29dc110f468834062faaeb527ac7
Florian <hohberger.florian@web.de>
2022-02-23 13:35:30 +0100

    Projektdateien hinzufügen.

---
+++ DaimlerTSS_Coding_Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DaimlerTSS_Coding_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
